0f82147635cc8a4f7683f5be94e640019a6182be
AdamIx <adamburdik9@gmail.com>
2025-07-18 21:39:19 +0200

    Add: getServers request to examples

--- Examples/src/main/java/me/adamix/sonoran/examples/Main.java
+++ Examples/src/main/java/me/adamix/sonoran/examples/Main.java
@@ -2,8 +2,11 @@ package me.adamix.sonoran.examples;
 
 import me.adamix.sonoran.Sonoran;
 import me.adamix.sonoran.cad.SonoranCad;
-import me.adamix.sonoran.cad.data.CadAccount;
+import me.adamix.sonoran.cad.data.general.CadAccount;
 import me.adamix.sonoran.cad.data.Result;
+import me.adamix.sonoran.cad.data.general.CadServer;
+
+import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
@@ -26,6 +29,19 @@ public class Main {
 				default -> throw new IllegalStateException("Unexpected value: " + result);
 			}
 		}));
+
+		cad.getServers((result) -> {
+			switch (result) {
+				case Result.Success<List<CadServer>> success -> {
+					for (CadServer cadServer : success.value()) {
+						System.out.println("Server: " + cadServer);
+					}
+				}
+				case Result.Error<List<CadServer>> error -> System.out.println(error.message());
+				case Result.Exception<List<CadServer>> exception -> throw new RuntimeException(exception.exception());
+				default -> throw new IllegalStateException("Unexpected value: " + result);
+			}
+		});
 
 		sonoran.shutdown();
 	}
