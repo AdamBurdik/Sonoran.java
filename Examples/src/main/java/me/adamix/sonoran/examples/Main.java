29ed48dbbf51646bde6ce2e701940dc4a1750791
AdamIx <adamburdik9@gmail.com>
2025-07-18 21:15:32 +0200

    Update: example to handle errors

--- Examples/src/main/java/me/adamix/sonoran/examples/Main.java
+++ Examples/src/main/java/me/adamix/sonoran/examples/Main.java
@@ -2,6 +2,8 @@ package me.adamix.sonoran.examples;
 
 import me.adamix.sonoran.Sonoran;
 import me.adamix.sonoran.cad.SonoranCad;
+import me.adamix.sonoran.cad.data.CadAccount;
+import me.adamix.sonoran.cad.data.Result;
 
 public class Main {
 	public static void main(String[] args) {
@@ -16,8 +18,13 @@ public class Main {
 
 		SonoranCad cad = sonoran.cad();
 
-		cad.getAccount(username, (cadAccount -> {
-			System.out.println("Account: " + cadAccount);
+		cad.getAccount(username, (result -> {
+			switch (result) {
+				case Result.Success<CadAccount> success -> System.out.println("Account: " + success.value());
+				case Result.Error<CadAccount> error -> System.out.println(error.message());
+				case Result.Exception<CadAccount> exception -> throw new RuntimeException(exception.exception());
+				default -> throw new IllegalStateException("Unexpected value: " + result);
+			}
 		}));
 
 		sonoran.shutdown();
