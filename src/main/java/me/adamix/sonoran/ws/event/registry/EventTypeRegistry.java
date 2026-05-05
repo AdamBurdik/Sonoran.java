e976e2e25841d1b83a25c63fcfb9e3efc0cbe85d
adamix <adamburdik9@gmail.com>
2026-05-05 19:34:56 +0200

    Fix: empty body of method

--- src/main/java/me/adamix/sonoran/ws/event/registry/EventTypeRegistry.java
+++ src/main/java/me/adamix/sonoran/ws/event/registry/EventTypeRegistry.java
@@ -2,20 +2,23 @@ package me.adamix.sonoran.ws.event.registry;
 
 import me.adamix.sonoran.ws.event.CommunityLinkVerifiedEvent;
 import me.adamix.sonoran.ws.event.WSEvent;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class EventTypeRegistry {
-    private final Map<String, Class<? extends WSEvent>> eventTypes = Map.of(
-            "EVENT_COMMUNITY_LINK_VERIFIED", CommunityLinkVerifiedEvent.class
-    );
+    private final Map<String, Class<? extends WSEvent>> eventTypes = new HashMap<>();
 
-    @ApiStatus.Internal
+    public EventTypeRegistry() {
+        eventTypes.putAll(Map.of(
+                "EVENT_COMMUNITY_LINK_VERIFIED", CommunityLinkVerifiedEvent.class
+        ));
+    }
+
     public void register(@NotNull String key, @NotNull Class<? extends WSEvent> clazz) {
-
+        eventTypes.put(key, clazz);
     }
 
     public @Nullable Class<? extends WSEvent> getType(@NotNull String key) {
